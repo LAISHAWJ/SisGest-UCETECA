16f9ce99bfd0c16862d36570abf448d33f4d46dc
franluv1794 <solanofrancisluz@gmail.com>
2025-12-23 13:20:50 -0400

    Implementación de la función prestar_libro()

--- GestUCETECA/main/main.py
+++ GestUCETECA/main/main.py
@@ -1,4 +1,4 @@
-# main.py - Sistema de Gestión de Biblioteca
+# main.py - Sistema de Gestiï¿½n de Biblioteca
 class Biblioteca:
     def __init__(self):
         self.libros = []
@@ -15,8 +15,20 @@ class Biblioteca:
             estado = "Disponible" if libro["disponible"] else "Prestado"
             print(f"- {libro['titulo']} por {libro['autor']} [{estado}]")
 
+    def prestar_libro(self, titulo, id_usuario):
+        for libro in self.libros:
+            if libro["titulo"] == titulo and libro["disponible"]:
+                libro["disponible"] = False
+                print(f"Libro '{titulo}' prestado al usuario {id_usuario}")
+                return True
+
+        print("Libro no disponible")
+        return False
+
+
+
 if __name__ == "__main__":
     biblioteca = Biblioteca()
-    biblioteca.agregar_libro("Cien Años de Soledad", "Gabriel García Márquez")
+    biblioteca.agregar_libro("Cien Aï¿½os de Soledad", "Gabriel Garcï¿½a Mï¿½rquez")
     biblioteca.agregar_libro("Don Quijote", "Miguel de Cervantes")
     biblioteca.listar_libros()
